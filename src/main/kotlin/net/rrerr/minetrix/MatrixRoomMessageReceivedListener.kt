648d05a9a014a48025e745a0cb82317d4686ec8a
Dominik Kwiatek <dom@dom.dog>
2019-09-01 19:48:03 +0100

    Forgot the _matrix

--- src/main/kotlin/net/rrerr/minetrix/MatrixRoomMessageReceivedListener.kt
+++ src/main/kotlin/net/rrerr/minetrix/MatrixRoomMessageReceivedListener.kt
@@ -80,5 +80,5 @@ class MatrixRoomMessageReceivedListener(private val plugin: Main) : RoomMessageR
     }
 
     private fun getMediaUrl(message: UrlMessageContent) =
-        "${plugin.matrixClient!!.hs.base}/media/v1/download/${message.url!!.toString().replace("mxc://", "")}"
+        "${plugin.matrixClient!!.hs.base}/_matrix/media/v1/download/${message.url!!.toString().replace("mxc://", "")}"
 }
