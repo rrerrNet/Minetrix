269cba43cc38d01c292a2a0b17401a05bfc2d9ff
Dominik Kwiatek <dom@dom.dog>
2019-09-01 17:22:42 +0100

    Add File/Image message handling

--- src/main/kotlin/net/rrerr/minetrix/MatrixRoomMessageReceivedListener.kt
+++ src/main/kotlin/net/rrerr/minetrix/MatrixRoomMessageReceivedListener.kt
@@ -1,7 +1,14 @@
 package net.rrerr.minetrix
 
+import de.msrd0.matrix.client.event.FileMessageContent
+import de.msrd0.matrix.client.event.ImageMessageContent
+import de.msrd0.matrix.client.event.MessageTypes
+import de.msrd0.matrix.client.event.UrlMessageContent
 import de.msrd0.matrix.client.listener.RoomMessageReceivedEvent
 import de.msrd0.matrix.client.listener.RoomMessageReceivedListener
+import net.md_5.bungee.api.chat.ClickEvent
+import net.md_5.bungee.api.chat.TextComponent
+import org.apache.commons.io.FileUtils
 
 class MatrixRoomMessageReceivedListener(private val plugin : Main) : RoomMessageReceivedListener {
     override fun call(event: RoomMessageReceivedEvent): Boolean {
@@ -13,7 +20,33 @@ class MatrixRoomMessageReceivedListener(private val plugin : Main) : RoomMessage
             return true
         }
 
+        if (event.msg.content.msgtype == MessageTypes.IMAGE) {
+            val messageContent = event.msg.content as ImageMessageContent
+            val imageType = messageContent.mimetype.split('/').last().toUpperCase()
+            val url = getMediaUrl(plugin, messageContent)
+            sendLinkMessage(plugin, url, "Image [${messageContent.width}x${messageContent.height}, $imageType]")
+            return true
+        }
+
+        if (event.msg.content.msgtype == MessageTypes.FILE) {
+            val messageContent = event.msg.content as FileMessageContent
+            val url = getMediaUrl(plugin, messageContent)
+            val size = FileUtils.byteCountToDisplaySize(messageContent.size!!.toLong())
+            sendLinkMessage(plugin, url, "File [${messageContent.filename}, $size]")
+            return true
+        }
+
         plugin.server.broadcastMessage("[§2${event.msg.sender}§r] ${event.msg.body}")
         return true
+    }
+
+    private fun sendLinkMessage(plugin: Main, url: String, label: String) {
+        val chatLink = TextComponent(label)
+        chatLink.clickEvent = ClickEvent(ClickEvent.Action.OPEN_URL, url)
+        plugin.server.onlinePlayers.forEach { it.spigot().sendMessage(chatLink) }
+    }
+
+    private fun getMediaUrl(plugin: Main, message: UrlMessageContent): String {
+        return "${plugin.matrixClient!!.hs.base}media/v1/download${message.url!!.mediaId}"
     }
 }
