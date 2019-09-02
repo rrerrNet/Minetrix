1223c8948400b3013481fb40762e3cb4581ef917
Dominik Kwiatek <dom@dom.dog>
2019-09-02 19:39:02 +0100

    Add click event for automatically writing a user's Matrix handle in the chat

--- src/main/kotlin/net/rrerr/minetrix/MatrixRoomMessageReceivedListener.kt
+++ src/main/kotlin/net/rrerr/minetrix/MatrixRoomMessageReceivedListener.kt
@@ -64,6 +64,7 @@ class MatrixRoomMessageReceivedListener(private val plugin: Main) : RoomMessageR
         val value = ArrayList<BaseComponent>()
         val sender = TextComponent(message.sender.toString())
         sender.color = ChatColor.DARK_GREEN
+        sender.clickEvent = ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, message.sender.toString())
 
         if (plugin.useSenderTooltips) {
             val tooltip = ComponentBuilder(message.sender.displayname)
