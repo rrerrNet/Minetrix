c7f34113d6fe53e33fb3a1bbc0b0975e63c9a4cd
Georg Gadinger <nilsding@nilsding.org>
2019-10-08 20:37:52 +0200

    Announce Advancements

--- src/main/kotlin/net/rrerr/minetrix/PlayerEventListener.kt
+++ src/main/kotlin/net/rrerr/minetrix/PlayerEventListener.kt
@@ -4,6 +4,7 @@ import de.msrd0.matrix.client.event.TextMessageContent
 import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerAdvancementDoneEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 
@@ -35,6 +36,20 @@ class PlayerEventListener(plugin: Main) : ListenerBase(plugin) {
         }
 
         val message = "☠ ${ChatColor.stripColor(ev.deathMessage!!)}"
+        sendMessage(TextMessageContent(message))
+    }
+
+    @EventHandler
+    fun playerAdvancementDone(ev: PlayerAdvancementDoneEvent) {
+        // We only want to forward advancements that are done, are not some new unlocked recipes,
+        // and are not the start of some advancement chain.
+        if (!ev.player.getAdvancementProgress(ev.advancement).isDone ||
+            ev.advancement.key.key.startsWith("recipes/") ||
+            ev.advancement.key.key.endsWith("/root")) {
+            return
+        }
+
+        val message = "🏆 ${ev.player.name} has made the advancement ${ev.advancement.key.key}"
         sendMessage(TextMessageContent(message))
     }
 }
